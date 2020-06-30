c0a2f358ba7d667da89be034e106ec7ef66bb329
Alice Fang <alicesf2@illinois.edu>
2020-06-30 17:23:43 -0500

    Updates

---
+++ precache-manifest.25cc0cf56511cd17e73a032056fb722c.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92881e7e63e9b7c9ad16173fac21e8ae",
+    "url": "/fs-website/index.html"
+  },
+  {
+    "revision": "c7d56f66f745ccfac059",
+    "url": "/fs-website/static/css/main.18c60065.chunk.css"
+  },
+  {
+    "revision": "591c5f5f6df244a73f5e",
+    "url": "/fs-website/static/js/2.3e5d0c3f.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/fs-website/static/js/2.3e5d0c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7d56f66f745ccfac059",
+    "url": "/fs-website/static/js/main.4e30b835.chunk.js"
+  },
+  {
+    "revision": "b3dc0c5a927e7d5924b3",
+    "url": "/fs-website/static/js/runtime-main.5186909c.js"
+  },
+  {
+    "revision": "7faa0bbd3e2ffae9bc6ebf65a37d240d",
+    "url": "/fs-website/static/media/aaa-logo.7faa0bbd.png"
+  },
+  {
+    "revision": "09d36716d11af8c081acb93ea768add0",
+    "url": "/fs-website/static/media/about-bg.09d36716.png"
+  },
+  {
+    "revision": "a951c26f517f8bfb134498d229c0ae58",
+    "url": "/fs-website/static/media/anjolee.a951c26f.jpeg"
+  },
+  {
+    "revision": "7baa98ad677c17a3ac402f4f6e70005c",
+    "url": "/fs-website/static/media/asha.7baa98ad.png"
+  },
+  {
+    "revision": "4906c4b035628d83bcb90789310515d2",
+    "url": "/fs-website/static/media/bac-bg.4906c4b0.png"
+  },
+  {
+    "revision": "ca1a6810afad8a478cd76c8c0f0627ad",
+    "url": "/fs-website/static/media/faith.ca1a6810.png"
+  },
+  {
+    "revision": "38a66c556bd72e3a562bffa777c769e1",
+    "url": "/fs-website/static/media/home.38a66c55.png"
+  },
+  {
+    "revision": "c5319c963fed5bbc7f250191c2aa69ac",
+    "url": "/fs-website/static/media/home1.c5319c96.png"
+  },
+  {
+    "revision": "42789610fd33439a16eafff9b238fe5b",
+    "url": "/fs-website/static/media/home2.42789610.png"
+  },
+  {
+    "revision": "02680a8396d162c74d35570039b2999d",
+    "url": "/fs-website/static/media/home3.02680a83.png"
+  },
+  {
+    "revision": "6225137f0ef41526187e28e041fad188",
+    "url": "/fs-website/static/media/jasmine.6225137f.jpg"
+  },
+  {
+    "revision": "bc365d3f796f3c2ac6ec2cc1a561a0e4",
+    "url": "/fs-website/static/media/jason.bc365d3f.jpg"
+  },
+  {
+    "revision": "04f56b78df4811fcf2fdbc64d533b80d",
+    "url": "/fs-website/static/media/julia.04f56b78.jpg"
+  },
+  {
+    "revision": "3fa7bdc1dfaa1d901a0817f4ca523c6a",
+    "url": "/fs-website/static/media/margaret.3fa7bdc1.jpeg"
+  },
+  {
+    "revision": "c20a2410ee3301d236ed63fadd9e2eeb",
+    "url": "/fs-website/static/media/marielle.c20a2410.jpg"
+  },
+  {
+    "revision": "a2773337c59a84a702dca1bcfe0efc54",
+    "url": "/fs-website/static/media/mary.a2773337.jpg"
+  },
+  {
+    "revision": "276b6b17d8ff65d4113e3656718e8ba5",
+    "url": "/fs-website/static/media/michael.276b6b17.jpg"
+  },
+  {
+    "revision": "518d5ada933b1d1f4f99fff92a808a4f",
+    "url": "/fs-website/static/media/michelle.518d5ada.jpeg"
+  },
+  {
+    "revision": "f7d07e8c1ae4aa93636341e1b7dee9ba",
+    "url": "/fs-website/static/media/oc-bg.f7d07e8c.png"
+  },
+  {
+    "revision": "b6f96284010955ed6fcb6361cc589574",
+    "url": "/fs-website/static/media/pfs-bg.b6f96284.png"
+  },
+  {
+    "revision": "c43c6175825d75c8888876062504cf3b",
+    "url": "/fs-website/static/media/rafy.c43c6175.jpg"
+  },
+  {
+    "revision": "8ba079684ec78a326debe33118d25ada",
+    "url": "/fs-website/static/media/vicki.8ba07968.jpg"
+  }
+]);
--- src/data/promo.js
+++
@@ -1,5 +0,0 @@
-export default [
-  "https://youtube.com/embed/ogemwbCg_gc",
-  "https://youtube.com/embed/b-5z3Mgfldo",
-  "https://youtube.com/embed/rwQ_RDhKit8",
-];
