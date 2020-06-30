302448488184e130c2bd2b5e2408c4a2e7314eb3
Alice Fang <alicesf2@illinois.edu>
2020-06-30 16:40:35 -0500

    deploy to gh pages

--- src/data/pastFS.js
+++ src/data/pastFS.js
@@ -1,16 +1,16 @@
 export default [
   {
-    src: "https://youtube.com/embed/yUCAtBLXZew",
+    src: "https://youtube.com/embed/OzDBaFNrqIc",
     name: "FS33: NEW BEGINNINGS",
     headCoords: ["Preston Lee", "Chelsea Shang"],
   },
   {
-    src: "https://www.youtube.com/embed/ZpKu-78hmIk",
+    src: "https://youtube.com/embed/yUCAtBLXZew",
     name: "FS32: CROSSING POINT",
     headCoords: ["Annie Jiang", "Clara Yam"],
   },
   {
-    src: "https://youtube.com/embed/OzDBaFNrqIc",
+    src: "https://www.youtube.com/embed/ZpKu-78hmIk",
     name: "FS31: NEXT CHAPTER",
     headCoords: ["Ian Chou", "Alice Yang"],
   },
